1a03e73ef040b968cd5f51aefc7913c1f60ba771
alintulu <adelina.eleonora.lintuluoto@cern.ch>
2020-04-23 09:49:38 +0200

    add semicolon lol

--- JetAnalyzers/bin/jet_synchfit_x.cc
+++ JetAnalyzers/bin/jet_synchfit_x.cc
@@ -573,7 +573,7 @@ int main(int argc,char**argv){
 
    // Create the txt file from the fitResults vector
    //TString txtFilename = outputDir+era+"_L1FastJet_"+((algo1.EqualTo(algo2)) ? JetInfo(algo12).alias : algo12)+".txt";
-   TString txtFilename = "CondFormats/JetMETObjects/data/" + era + "_L1FastJet.txt" //+ get_algorithm_suffix(aalgo1) + ".txt"; // TString txtFilename = "parameters_"+algo12+".txt";
+   TString txtFilename = "CondFormats/JetMETObjects/data/" + era + "_L1FastJet.txt"; //+ get_algorithm_suffix(aalgo1) + ".txt"; // TString txtFilename = "parameters_"+algo12+".txt";
    createTxtFile(txtFilename, fitResults);
    
    cout<<"\t Wrote output..." << endl;
